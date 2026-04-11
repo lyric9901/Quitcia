f918c5b8fcb5618e232ed915209b5b979f7949f5
lyric9901 <shabanabano.05r@gmail.com>
2026-04-12 00:22:57 +0530

    yo

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.urge.relief',
-  appName: 'urge-relief',
-  webDir: 'out'
-};
-
-export default config;
